d526937f7107ac9e813088acf35e444f04674230
Jason Vega <javega3@illinois.edu>
2023-12-19 17:47:41 +0000

    Made installing Llama models optional

--- install.sh
+++ install.sh
@@ -5,19 +5,29 @@ git clone https://github.com/facebookresearch/llama.git
 mv llama llama_repo
 cd llama_repo
 pip install -e .
-chmod u+x download.sh
-echo "Downloading Llama 2 models..."
-./download.sh
+
+read -p "Download Llama 2 models? (y/n): " choice
+
+if [ "$choice" = "y" ]; then
+    chmod u+x download.sh
+    ./download.sh
+fi
+
 cd ..
 
 # Move Llama 2 files to correct place
 mv llama_repo/llama .
 mv generation_attack.py llama
+rm -rf llama_repo
 
 # Install Llama Guard
-git clone https://github.com/facebookresearch/PurpleLlama.git
-cd PurpleLlama/Llama-Guard
-chmod u+x download.sh
-echo "Downloading Llama Guard model..."
-./download.sh
-cd ../..
+read -p "Download Llama Guard model? (y/n): " choice
+
+if [ "$choice" = "y" ]; then
+    git clone https://github.com/facebookresearch/PurpleLlama.git
+    cd PurpleLlama/Llama-Guard
+    chmod u+x download.sh
+    ./download.sh
+    cd ../..
+    rm -rf PurpleLlama
+fi
